2a04f97166d0ea85505544332c8164b3328203d5
Pancc123 <442654066@qq.com>
2020-05-06 19:31:03 +0800

    新增定时器

---
+++ socket_link/timer.py
@@ -0,0 +1,32 @@
+import threading
+from datetime import datetime
+import time
+
+exec_count=0
+
+
+def heart_beat(time_cycle):
+    print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    global exec_count
+    exec_count += 1
+    # 15秒后停止定时器
+    if exec_count < time_cycle:
+        threading.Timer(1, heart_beat,args=(time_cycle,)).start()
+
+
+cancel_tmr = False
+
+
+def heart_beat1():
+    print
+    time.strftime('%Y-%m-%d %H:%M:%S')
+
+    if not cancel_tmr:
+        threading.Timer(1, heart_beat).start()
+
+
+heart_beat1()
+
+# 15秒后停止定时器
+time.sleep(15)
+cancel_tmr = True
